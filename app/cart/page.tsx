d3966543b77abaca947a9523cbc48473a6a02f9e
erwin <erwinmarin05@gmail.com>
2026-04-23 18:40:34 +0000

    done dark and ligth theme

--- app/cart/page.tsx
+++ app/cart/page.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 function CartPage() {
-  return <div></div>;
+  return <div>cart</div>;
 }
 
 export default CartPage;
--- components/navbar/NavSearch.tsx
+++ components/navbar/NavSearch.tsx
@@ -1,7 +1,13 @@
-import React from "react";
+import React from 'react'
+import { Input } from '../ui/input'
 
 function NavSearch() {
-  return <div>NavSearch</div>;
+  return (
+    <div>
+      <Input type='search' placeholder='search product' className='max-w-xs dark:bg-muted'/>
+    </div>
+  )
 }
 
-export default NavSearch;
+export default NavSearch
+
