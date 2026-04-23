98dd753a6b251851549f2c904a713d0d82c5faa8
erwin <erwinmarin05@gmail.com>
2026-04-23 19:04:04 +0000

    done dropdown menu

--- app/products/page.tsx
+++ app/products/page.tsx
@@ -4,4 +4,4 @@ function ProductsPage() {
   return <div>ProductsPage</div>;
 }
 
-export default ProductPage;
+export default ProductsPage;
--- components/navbar/LinksDropdown.tsx
+++ components/navbar/LinksDropdown.tsx
@@ -1,7 +1,37 @@
 import React from "react";
+import {
+  DropdownMenu,
+  DropdownMenuTrigger,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuSeparator,
+} from "@/components/ui/dropdown-menu";
+import { LuAlignLeft } from "react-icons/lu";
+import Link from "next/link";
+import { Button } from "../ui/button";
+import { links } from "@/utils/links";
 
 function LinksDropdown() {
-  return <div>LinksDropdown</div>;
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger>
+        <Button variant="outline" className="flex gap-4 max-w-[100px]">
+          <LuAlignLeft className="w-10 h-6" />
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent>
+        {links.map((link) => {
+          return (
+            <DropdownMenuItem key={link.href}>
+              <Link href={link.href} className="capitalize w-full">
+              {link.label}
+              </Link>
+            </DropdownMenuItem>
+          );
+        })}
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
 }
 
 export default LinksDropdown;
